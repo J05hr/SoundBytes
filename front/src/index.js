7b9541efc5428ac4018e9017d8a23e249f1d9bb9
J05h <J05h@protonmail.com>
2020-11-21 13:20:52 -0500

    setup basic framework

---
+++ front/src/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import './styles/index.css';
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
---
+++ front/src/utils/utils.js
@@ -0,0 +1,9 @@
+
+
+export async function func (param) {
+    if (param != null){
+        return false;
+    }else{
+        return false;
+    }
+}
