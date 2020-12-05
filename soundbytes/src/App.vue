c69b22cff4b8dba05a9803fd70abcf795d2b92f2
J05h <J05h@protonmail.com>
2020-12-05 17:15:38 -0500

    Framework setup

---
+++ soundbytes/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view/>
+</template>
+
+
+<script>
+export default {
+  name: "SoundBytes",
+  props: {},
+};
+</script>
+
+
+<style>
+@import "~bulma/css/bulma.css";
+@import "./styles/app.css";
+@import "./styles/text.css";
+</style>
